8a9efe6b3a7917709e0c93f23f297d455b75ee31
Yassin AJDI <yassin.ajdi@gmail.com>
2018-12-18 21:31:24 +0000

    Add pending intent to open recipe atctivity

--- app/src/main/java/com/ajdi/yassin/bakingapp/RecipeWidget.java
+++ app/src/main/java/com/ajdi/yassin/bakingapp/RecipeWidget.java
@@ -1,9 +1,13 @@
 package com.ajdi.yassin.bakingapp;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
+
+import com.ajdi.yassin.bakingapp.ui.list.RecipeListActivity;
 
 /**
  * Implementation of App Widget functionality.
@@ -13,10 +17,15 @@ public class RecipeWidget extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        CharSequence widgetText = "Yassin AJDI";
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.recipe_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        views.setTextViewText(R.id.appwidget_text, widgetText + " " + String.valueOf(appWidgetId));
+
+        // When widget is clicked open recipe list activity
+        Intent intent = new Intent(context, RecipeListActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
