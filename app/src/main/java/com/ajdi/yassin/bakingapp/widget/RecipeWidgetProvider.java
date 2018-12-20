b63e61690769511d7ab62969d103630de8ba7daf
Yassin AJDI <yassin.ajdi@gmail.com>
2018-12-20 16:57:11 +0000

    Add click listner to widget container

--- app/src/main/java/com/ajdi/yassin/bakingapp/widget/RecipeWidgetProvider.java
+++ app/src/main/java/com/ajdi/yassin/bakingapp/widget/RecipeWidgetProvider.java
@@ -1,13 +1,20 @@
 package com.ajdi.yassin.bakingapp.widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.widget.RemoteViews;
 
 import com.ajdi.yassin.bakingapp.R;
+import com.ajdi.yassin.bakingapp.ui.recipedetail.RecipeDetailsActivity;
+import com.ajdi.yassin.bakingapp.ui.recipelist.RecipeListActivity;
+import com.ajdi.yassin.bakingapp.utils.Constants;
+
+import static android.content.Context.MODE_PRIVATE;
 
 /**
  * Implementation of App Widget functionality.
@@ -34,28 +41,26 @@ public class RecipeWidgetProvider extends AppWidgetProvider {
         remoteViews.setEmptyView(R.id.widget_list, R.id.empty_view);
 
         // retrieve recipe data from sharedPreferences
-//        SharedPreferences prefs = context.getSharedPreferences(Constants.MY_PREFS_NAME, MODE_PRIVATE);
-//        String recipeName = prefs.getString(context.getString(R.string.recipe_name), "No Recipe Added");
+        SharedPreferences prefs = context.getSharedPreferences(Constants.MY_PREFS_NAME, MODE_PRIVATE);
+        String recipeName = prefs.getString(context.getString(R.string.recipe_name), "No Recipe Added");
 //        String ingredients = prefs.getString(context.getString(R.string.ingredients), "Nothing!!");
-//        long recipeId = prefs.getLong(context.getString(R.string.recipe_id), -1);
+        long recipeId = prefs.getLong(context.getString(R.string.recipe_id), -1);
 
-        // Update remote views
-//        views.setTextViewText(R.id.appwidget_recipe_name, recipeName);
-//        views.setTextViewText(R.id.appwidget_ingredient, ingredients);
+        // Update recipe name
+        remoteViews.setTextViewText(R.id.widget_recipe_name, recipeName);
 
-        // Add pending intent to this widget item
         // attach an on-click listener to the widget
-//        Intent intent;
-//        if (recipeId >= 0) {
-//            intent = new Intent(context, RecipeDetailsActivity.class);
-//            intent.putExtra(context.getString(R.string.recipe_id), recipeId);
-//        } else {
-//            intent = new Intent(context, RecipeListActivity.class);
-//        }
+        Intent recipeIntent;
+        if (recipeId >= 0) {
+            recipeIntent = new Intent(context, RecipeDetailsActivity.class);
+            recipeIntent.putExtra(context.getString(R.string.recipe_id), recipeId);
+        } else {
+            recipeIntent = new Intent(context, RecipeListActivity.class);
+        }
 //        intent.setAction()
 //        int flags = Intent.FLAG_ACTIVITY_NEW_TASK;
-//        PendingIntent pendingIntent = PendingIntent.getActivity(context, appWidgetId, intent, 0);
-//        views.setOnClickPendingIntent(R.id.widget_list_container, pendingIntent);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, appWidgetId, recipeIntent, 0);
+        remoteViews.setOnClickPendingIntent(R.id.widget_list_container, pendingIntent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
