132b6d4a71d29d5afab76c6be6c8a3824c7a7a96
Yassin AJDI <yassin.ajdi@gmail.com>
2018-12-21 17:23:52 +0000

    Simple first test

--- app/src/androidTest/java/com/ajdi/yassin/bakingapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/ajdi/yassin/bakingapp/ExampleInstrumentedTest.java
@@ -1,25 +1,26 @@
 package com.ajdi.yassin.bakingapp;
 
 import android.content.Context;
-import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
-import static org.junit.Assert.*;
+import androidx.test.core.app.ApplicationProvider;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
+@RunWith(JUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = ApplicationProvider.getApplicationContext();
 
         assertEquals("com.ajdi.yassin.bakingapp", appContext.getPackageName());
     }
--- app/src/androidTest/java/com/ajdi/yassin/bakingapp/ui/recipelist/RecipeListActivityTest.java
+++ app/src/androidTest/java/com/ajdi/yassin/bakingapp/ui/recipelist/RecipeListActivityTest.java
@@ -1,11 +1,24 @@
 package com.ajdi.yassin.bakingapp.ui.recipelist;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
 import static org.junit.Assert.*;
 
 /**
  * @author Yassin Ajdi
  * @since 12/21/2018.
  */
+@RunWith(JUnit4.class)
 public class RecipeListActivityTest {
 
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
 }
