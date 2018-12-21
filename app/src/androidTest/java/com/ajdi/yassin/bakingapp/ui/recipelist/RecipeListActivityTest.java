a368a26459655fc12e7c42c338e108e946ee3ed5
Yassin AJDI <yassin.ajdi@gmail.com>
2018-12-21 13:53:45 +0000

    Setup basic espresso tests

---
+++ app/src/androidTest/java/com/ajdi/yassin/bakingapp/ui/recipelist/RecipeListActivityTest.java
@@ -0,0 +1,11 @@
+package com.ajdi.yassin.bakingapp.ui.recipelist;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Yassin Ajdi
+ * @since 12/21/2018.
+ */
+public class RecipeListActivityTest {
+
+}
