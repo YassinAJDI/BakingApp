95b268f2da8cc0233f36bbd90862dd2238a0db65
Yassin AJDI <yassin.ajdi@gmail.com>
2018-12-22 14:41:47 +0000

    Add UI tests

--- app/src/androidTest/java/com/ajdi/yassin/bakingapp/ui/recipelist/RecipeListActivityTest.java
+++ app/src/androidTest/java/com/ajdi/yassin/bakingapp/ui/recipelist/RecipeListActivityTest.java
@@ -1,7 +1,11 @@
 package com.ajdi.yassin.bakingapp.ui.recipelist;
 
+import android.view.View;
+
 import com.ajdi.yassin.bakingapp.R;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -9,17 +13,22 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.test.espresso.Espresso;
-import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.espresso.IdlingRegistry;
+import androidx.test.espresso.matcher.BoundedMatcher;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
 import static androidx.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
+import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.CoreMatchers.is;
 
 /**
  * @author Yassin Ajdi
@@ -29,8 +38,8 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 @LargeTest
 public class RecipeListActivityTest {
 
+    private static final String RECIPE_NAME = "Brownies";
     private static final int MIN_RECIPE_COUNT = 4;
-    private static final int SECOND_POSITION = 1;
 
     @Rule
     public ActivityTestRule<RecipeListActivity> mActivityRule =
@@ -38,37 +47,69 @@ public class RecipeListActivityTest {
 
     @Before
     public void setUp() throws Exception {
-        Espresso.registerIdlingResources(
-                mActivityRule.getActivity().getCountingIdlingResource());
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        Espresso.unregisterIdlingResources(
+        IdlingRegistry.getInstance().register(
                 mActivityRule.getActivity().getCountingIdlingResource());
     }
 
     @Test
-    public void onRecipeListActivityOpen_displayRecyclerView() {
+    public void onLaunch_recipeListIsDisplayed() {
         // Check that the Recycler View is  displayed
         onView(withId(R.id.rv_recipe_list)).check(matches(isDisplayed()));
     }
 
     @Test
-    public void changeText_sameActivity() {
+    public void onLaunch_recipeListHasMinimumFourRecipe() {
         // check recyclerview has enough recipes
         onView(withId(R.id.rv_recipe_list)).check(matches(hasMinimumChildCount(MIN_RECIPE_COUNT)));
-
-//        onView(withId(R.id.rv_recipe_list)).perform(actionOnItemAtPosition(SECOND_POSITION, click()));
     }
 
     @Test
     public void clickRecipeListItem_openRecipeDetailsActivity() {
 
         onView(withId(R.id.rv_recipe_list))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
+                .perform(actionOnItemAtPosition(1, click()));
 
         onView(withId(R.id.rv_ingredients))
                 .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void performClicksOnTwoRecipeItems_thenClickBack() {
+
+        onView(withId(R.id.rv_recipe_list)).perform(
+                actionOnItemAtPosition(0, click()));
+        Espresso.pressBack();
+
+        onView(withId(R.id.rv_recipe_list)).perform(
+                actionOnItemAtPosition(1, click()));
+        Espresso.pressBack();
+    }
+
+    @Test
+    public void clickRecipe_checkToolbarTitle() {
+        onView(withId(R.id.rv_recipe_list)).perform(actionOnItemAtPosition(1, click()));
+        onView(isAssignableFrom(Toolbar.class)).check(matches(getToolbarTitle(is(RECIPE_NAME))));
+    }
+
+    private Matcher<? super View> getToolbarTitle(final Matcher<String> stringMatcher) {
+        return new BoundedMatcher<Object, Toolbar>(Toolbar.class) {
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Toolbar title: ");
+                stringMatcher.describeTo(description);
+            }
+
+            @Override
+            protected boolean matchesSafely(Toolbar item) {
+                return stringMatcher.matches(item.getTitle());
+            }
+        };
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        IdlingRegistry.getInstance().unregister(
+                mActivityRule.getActivity().getCountingIdlingResource());
     }
 }
